05a6af40c4b2a82008ef9d2001ef51015b78e9d6
Jordan Heale <webmaster@pridit.co.uk>
2022-12-22 18:47:33 +0000

    fix: remove redundant div

--- error.vue
+++ error.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <div class="flex items-center justify-center min-h-screen p-6 md:flex">
-        <div class="text-center">
-          <h1 class="mb-12 font-semibold text-9xl" v-if="error.statusCode == 404">404</h1>
-          <h1 class="mb-12 text-4xl" v-else>Internal Server Error</h1>
+  <NuxtLayout>
+    <div class="flex items-center justify-center min-h-screen p-6 md:flex">
+      <div class="text-center">
+        <h1 class="mb-12 font-semibold text-9xl" v-if="error.statusCode == 404">404</h1>
+        <h1 class="mb-12 text-4xl" v-else>Internal Server Error</h1>
 
-          <p class="mb-4">We could not find what you were looking for.</p>
+        <p class="mb-4">We could not find what you were looking for.</p>
 
-          <NuxtLink to="/">Back to the home page</NuxtLink>
-        </div>
+        <NuxtLink to="/">Back to the home page</NuxtLink>
       </div>
-    </NuxtLayout>
-  </div>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script>
