6de3a2309fa7abed6beabc0e915eaacf5d4bb7e9
Jordan Heale <webmaster@pridit.co.uk>
2022-12-25 15:23:36 +0000

    feat: use component on error template to adjust head title

--- error.vue
+++ error.vue
@@ -1,5 +1,9 @@
 <template>
   <NuxtLayout>
+    <Head>
+      <Title>{{ error.statusCode == 404 ? '404 Not Found' : error.statusCode }}</Title>
+    </Head>
+
     <div class="flex items-center justify-center min-h-screen p-6 md:flex">
       <div class="text-center">
         <h1 class="mb-12 font-semibold text-9xl" v-if="error.statusCode == 404">404</h1>
